0f65d09dbba1a63a3c09fc26a1f4e263f9e93328
Sujata Dev <sujatadev97@gmail.com>
2017-10-10 19:44:36 +0530

    formattings done

--- src/LoginStudent.pl
+++ src/LoginStudent.pl
@@ -1,4 +1,5 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
 use CGI::Carp;
@@ -10,16 +11,16 @@ my $database = "Qoncepts";
 my $dsn = "DBI:mysql:Qoncepts";
 my $userid = "root";
 my $pwd = "root";
-my $dbh = DBI -> connect($dsn, $userid, $pwd) or die $DBI::errstr;
+my $dbh = DBI -> connect($dsn, $userid, $pwd) or die;
 
 
-my $cgi = CGI->new();
+my $cgi = CGI -> new();
 
-my $newprn = $cgi->param('prn');
-my $newpass = $cgi->param('password');
-my ($name,$pass);
+my $newprn = $cgi -> param('prn');
+my $newpass = $cgi -> param('password');
+my ($name, $pass);
 
-print $cgi->header('text/html');
+print $cgi -> header('text/html');
 
 head_section();
 body_section();
@@ -27,41 +28,43 @@ body_section();
 
 sub head_section
 {
-    print "<html>
-    <head>
-    <meta charset='utf-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' href=
-        'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
-    <script
-        src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'>
-    </script>
-    <script src=
-        'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'>
-    </script>
-    <script src='https://code.jquery.com/jquery-1.10.2.js'></script>
-    <style>
-        body
-        {
-            background-image: url('quizbackground.jpeg');
-            color: white;
-            font-size: 30px;
-        }
-        .button,.btn
-        {
-            background-color: goldenrod;
-            border: 10px;
-            padding: 15px 20px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            color: black;
-            font-size: 16px;
-            margin: 20px;
-            box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-        }
-    </style>
-    </head>";
+    print "
+        <html>
+        <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' href=
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+        <script
+            src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'>
+        </script>
+        <script src=
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'>
+        </script>
+        <script src='https://code.jquery.com/jquery-1.10.2.js'></script>
+        <style>
+            body
+            {
+                background-image: url('quizbackground.jpeg');
+                color: white;
+                font-size: 30px;
+            }
+            .button,.btn
+            {
+                background-color: goldenrod;
+                border: 10px;
+                padding: 15px 20px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                color: black;
+                font-size: 16px;
+                margin: 20px;
+                box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0
+                            rgba(0,0,0,0.19);
+            }
+        </style>
+        </head>";
 }
 
 
@@ -72,27 +75,38 @@ sub body_section
         <div class='container'>
         <br><br><br><br><br>
         <center><b>";
-    my $flag=db_checking();
-    if($flag==1)
+    my $flag = db_checking();
+
+    if($flag == 1)
     {
         my $sth = $dbh -> prepare("select Name from Student
-                                        where PRN = \'$newprn\'");
-        $sth -> execute() or die $DBI::errstr;
+                                    where PRN = \'$newprn\'");
+        $sth -> execute() or die;
 
         while (my $row = $sth -> fetchrow_array())
         {
             $name = $row;
         }
         $sth -> finish();
+
         print "
             Welcome $name! <br><br>
-            <a href='Check_Quiz_List.htm' target='_top'>
-                <button class='button'><b> Check Allotted Quiz Lists </b></button>
-            </a>
-            <a href='Past_Quiz_Marks.htm' target='_top'>
-                <button class='button'><b> Check Marks of Quizzes </b></button>
-            </a>";
 
+            <form action='Check_Quiz_List.pl' method='GET'>
+                <input type='hidden' name='prn' value='$newprn'>
+            </form>
+
+            <a href='Check_Quiz_List.pl' target='_top'>
+                <button class='button'><b>
+                    Check Allotted Quiz Lists
+                </b></button>
+            </a>
+
+            <a href='Past_Quiz_Marks.htm' target='_top'>
+                <button class='button'><b>
+                    Check Marks of Quizzes
+                </b></button>
+            </a>";
     }
     else
     {
@@ -114,8 +128,8 @@ sub body_section
 sub db_checking
 {
     my $sth = $dbh -> prepare("select Password from Student
-                                        where PRN = \'$newprn\'");
-    $sth -> execute() or die $DBI::errstr;
+                                where PRN = \'$newprn\'");
+    $sth -> execute() or die;
 
     while (my $row = $sth -> fetchrow_array())
     {
@@ -132,4 +146,3 @@ sub db_checking
         return 0;
     }
 }
-
