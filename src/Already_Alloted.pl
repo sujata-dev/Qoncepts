738582906983f20e960bdfb4f54892f05955ff53
Sujata Dev <sujatadev97@gmail.com>
2017-10-27 13:17:46 +0530

    to notify if marks have already been allotted

---
+++ src/Already_Alloted.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/env perl
+
+# To notify if marks have already been allotted
+
+use strict;
+use warnings;
+use CGI::Carp;
+use CGI;
+use DBI;
+
+my $driver = "mysql";
+my $database = "Qoncepts";
+my $dsn = "DBI:mysql:Qoncepts";
+my $userid = "root";
+my $pwd = "root";
+my $dbh = DBI -> connect($dsn, $userid, $pwd) or die;
+
+
+my $cgi = CGI -> new();
+
+print $cgi -> header('text/html');
+
+head_section();
+body_section();
+
+
+sub head_section
+{
+    print "
+        <html>
+        <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' href=
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+        <script
+            src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'>
+        </script>
+        <script src=
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'>
+        </script>
+        <script src='https://code.jquery.com/jquery-1.10.2.js'></script>
+        <style>
+            body
+            {
+                background-image: url('quizbackground.jpeg');
+                color: white;
+            }
+            textarea,textarea.placeholder
+            {
+                color: black;
+                text-align: center;
+            }
+            .button,.btn
+            {
+                background-color: goldenrod;
+                border: 10px;
+                padding: 15px 20px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                color: black;
+                font-size: 16px;
+                margin: 20px;
+                box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0
+                            rgba(0,0,0,0.19);
+            }
+        </style>
+        </head>";
+}
+
+
+sub body_section
+{
+    print "
+        <body>
+        <br><br><br><br><br><br><br><br><br><br>
+        <div class='container'>
+            <center><h4><b>
+                You have already alloted marks to this student. <br><br>
+
+                <a href='Check_Quiz_Sheets_List.pl' target='_top'>
+                    <button class='button'><b>
+                        Return to Quiz Sheet List
+                    </b></button>
+                </a>
+            </center></b></h4>
+        </div>
+        </body>
+        </html>";
+}
