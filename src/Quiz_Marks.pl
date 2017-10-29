5636c21335c25352edda88e73180a4d5026f1f43
Sujata Dev <sujatadev97@gmail.com>
2017-10-29 17:30:04 +0530

    corrected display of marks

--- src/Quiz_Marks.pl
+++ src/Quiz_Marks.pl
@@ -20,7 +20,7 @@ my $cgi = CGI -> new();
 
 my $prn = $cgi -> param('prn');
 
-my ($testno, $marks, $flag);
+my ($testno, $sem, $subject, $marks, $maxmarks, $flag);
 
 print $cgi -> header('text/html');
 
@@ -69,12 +69,6 @@ sub head_section
                 box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0
                             rgba(0,0,0,0.19);
             }
-            #tab
-            {
-                -moz-tab-size: 4; /* Code for Firefox */
-                -o-tab-size: 4; /* Code for Opera 10.6-12.1 */
-                tab-size: 4;
-            }
 
         </style>
         </head>";
@@ -95,15 +89,23 @@ sub body_section
     while (my $row = $sth -> fetchrow_array())
     {
         $testno = $row;
-        my $sth1 = $dbh -> prepare("select sum(AllotedMarks) from QuizAnswersDB where AllotedMarks in (select AllotedMarks from QuizAnswersDB where PRN=? and TestNo=?)");
+        my $sth1 = $dbh -> prepare("select Semester, Subject,
+                                    sum(AllotedMarks), MaxMarks
+                                    from QuizAnswersDB natural join QuizDB
+                                    where AllotedMarks in
+                                    (select AllotedMarks
+                                    from QuizAnswersDB
+                                    where PRN=? and TestNo=?)");
         $sth1 -> execute($prn, $testno) or die;
-        while (my $row1 = $sth1 -> fetchrow_array())
+        while (($sem, $subject, $marks, $maxmarks) =
+                                            $sth1 -> fetchrow_array())
         {
-            $marks = $row1;
             print "
-                <b><br><br>
-                TestNo:&ensp;&ensp;$testno<br>
-                Marks:&ensp;&ensp;$marks<br><br></b>";
+                <b>
+                TestNo: $testno<br>
+                Semester: $sem<br>
+                Subject: $subject<br>
+                Marks: $marks/$maxmarks<br><br><br></b>";
 
             $flag = 1;
         }
